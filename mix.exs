99f6783c19d3818117de3461db6474d6baf47c9b
Poff Poffenberger <poffdeluxe@gmail.com>
2026-03-11 12:52:47 -0700

    Add Elixir project scaffold with Rustler dependency

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule MontyEx.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :monty_ex,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:rustler, "~> 0.37.0"}
+    ]
+  end
+end
